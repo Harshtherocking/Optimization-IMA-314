915b3214b4581741b20d8caefaca148765f35046
Ankur Majumdar <ankurmajumdar7891@gmail.com>
2024-10-16 18:28:58 +0530

    Tested on first_order.py

--- utils/first_order.py
+++ utils/first_order.py
@@ -4,18 +4,17 @@ from utils.base import Optim
 
 EPSILON = 0.001
 
-class GradientDecent (Optim) : 
+class GradientDecent (Optim): 
     '''
     Gradient Decent - description
     '''
-    def __init__ (self, alpha : float) -> None: 
+    def __init__ (self, alpha: float) -> None: 
         self.alpha = alpha
         return 
-    def _next (self, x : ndarray, grad_func) -> ndarray : 
+    def _next (self, x: ndarray, grad_func) -> ndarray: 
         return x - self.alpha * grad_func(x)
     
-    def optimize (self, x: ndarray, func_callback, grad_func_callback, grad_mod_callback) -> ndarray : 
+    def optimize (self, x: ndarray, func_callback, grad_func_callback, grad_mod_callback) -> ndarray: 
         while (grad_mod_callback(x) > EPSILON): 
             x = self._next(x, grad_func_callback)
         return x
-
