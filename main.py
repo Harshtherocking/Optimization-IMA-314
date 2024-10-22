gradient and hessian fixed

main.py
@@ -24,20 +24,23 @@ from utils.line_search import GoldenSearch
 if __name__ == "__main__":
     x = np.array([5,7])
 
-    # print(PiecewiseLinear.grad( np.array([1,1]) ))
-    # print(PiecewiseLinear.grad( np.array([-1,1]) ))
-    # print(PiecewiseLinear.grad( np.array([1,-1]) ))
-    # print(PiecewiseLinear.grad( np.array([-1,-1]) ))
-    # print(PiecewiseLinear.grad( np.array([0,1]) ))
-    # print(PiecewiseLinear.grad( np.array([1,0]) ))
-    # print(PiecewiseLinear.grad( np.array([-1,0]) ))
-    # print(PiecewiseLinear.grad( np.array([0,-1]) ))
-    # print(PiecewiseLinear.grad( np.array([0,0]) ))
 
-    # PiecewiseLinear.plot()
-
+    adam = Adam()
     sg = Subgradient()
-    print(PiecewiseLinear.optimize(x, optim =sg, is_plot = True))
+    f  = lambda x : x[0] ** 2 + x[0] * x[1] + x[1] ** 2
+    myfunc = Function(f)
+
+    print(PiecewiseLinear.optimize(x, optim= sg , is_plot= True))
+
+
+
+
+    # Rastrigin.plot([x])
+    # print(Rastrigin(x), Rastrigin.grad(x), Rastrigin.hessian(x), sep = "\n")
+    
+
+    # sg = Subgradient()
+    # print(PiecewiseLinear.optimize(x, optim =sg, is_plot = True))
 
 
     

utils/base.py
@@ -4,12 +4,14 @@ from abc import abstractmethod, ABC
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+EPS = 1e-8
+
 class Optim (ABC): 
     '''
     Base class for optimizers
     '''
     @abstractmethod
-    def optimize (self, x : ndarray, func_callback, grad_func_callback, grad_mod_callback, is_plot : bool) -> ndarray | tuple[ndarray,list[ndarray]]:
+    def optimize (self, x : ndarray, func_callback, grad_func_callback, hessian_func_callback, grad_mod_callback, is_plot : bool) -> ndarray | tuple[ndarray,list[ndarray]]:
         pass
 
 
@@ -19,9 +21,10 @@ class Function :
     func : function which should return : ndarray() object type
     grad : gradient function returning ndarray() obeject type
     '''
-    def __init__ (self, func , grad_func ,  name : str = "myFunc" ) -> None :
+    def __init__ (self, func , grad_func = None, hessian_func = None, name : str = "myFunc" ) -> None :
         self.__func = func
         self.__grad_func = grad_func
+        self.__hessian_func = hessian_func
         self.__name = name
         return None
 
@@ -31,15 +34,36 @@ class Function :
     def __repr__(self) -> str:
         return self.__name
 
-    def grad (self, x : ndarray) -> ndarray : 
-        return self.__grad_func(x)
+    def grad(self, x: np.ndarray) -> np.ndarray:
+        if self.__grad_func : 
+            return self.__grad_func(x)
+
+        _x, _y = x
+        df_dx = (self.__func([_x + EPS, _y]) - self.__func([_x - EPS, _y])) / (2 * EPS)
+        df_dy = (self.__func([_x, _y + EPS]) - self.__func([_x, _y - EPS])) / (2 * EPS)
+        return np.array([df_dx, df_dy])
+
+    def hessian(self, x: np.ndarray) -> np.ndarray:
+        if self.__hessian_func : 
+            return self.__hessian_func(x)
+
+        _x, _y = x
+        d2f_dx2 = (self.__func([_x + EPS, _y]) - 2 * self.__func([_x, _y]) + self.__func([_x - EPS, _y])) / (EPS ** 2)
+        d2f_dy2 = (self.__func([_x, _y + EPS]) - 2 * self.__func([_x, _y]) + self.__func([_x, _y - EPS])) / (EPS ** 2)
+        
+        d2f_dxdy = (self.__func([_x + EPS, _y + EPS]) - self.__func([_x + EPS, _y - EPS]) - 
+                     self.__func([_x - EPS, _y + EPS]) + self.__func([_x - EPS, _y - EPS])) / (4 * EPS ** 2)
+
+        hessian_matrix = np.array([[d2f_dx2, d2f_dxdy],
+                                    [d2f_dxdy, d2f_dy2]])
+        return hessian_matrix
 
     def grad_mod (self, x : ndarray) -> float : 
         l2_norm = np.sum(np.square(self.grad(x)))
         return np.sqrt(l2_norm)
 
     def optimize (self, initial_val : ndarray, optim: Optim, is_plot : bool = False) -> ndarray :
-        soln = optim.optimize(initial_val, self.__call__, self.grad, self.grad_mod, is_plot = is_plot) 
+        soln = optim.optimize(initial_val, self.__call__, self.grad, self.hessian, self.grad_mod, is_plot = is_plot) 
 
         if is_plot and isinstance(soln, tuple): 
             # plot the trajectory 

utils/first_order.py
@@ -29,7 +29,7 @@ class GradientDescent (Optim):
         # print(f"Alpha : {self.alpha}")
         return x - self.alpha * grad_func_callback(x)
 
-    def optimize (self, x: ndarray, func_callback, grad_func_callback, grad_mod_callback, is_plot : bool = False) -> ndarray | tuple[ndarray,list[ndarray]]: 
+    def optimize (self, x: ndarray, func_callback, grad_func_callback, hessian_func_callback, grad_mod_callback, is_plot : bool = False) -> ndarray | tuple[ndarray,list[ndarray]]: 
         plot_points : list[ndarray] = [x]
 
         while (grad_mod_callback(x) > EPSILON): 
@@ -71,7 +71,7 @@ class NesterovAcceleratedGradientDescent (Optim):
         return x - self.momentum
 
 
-    def optimize (self, x: ndarray, func_callback, grad_func_callback, grad_mod_callback, is_plot : bool = False) -> ndarray | tuple[ndarray,list[ndarray]]: 
+    def optimize (self, x: ndarray, func_callback, grad_func_callback, hessian_func_callback, grad_mod_callback, is_plot : bool = False) -> ndarray | tuple[ndarray,list[ndarray]]: 
         self.momentum = np.zeros(x.shape)
         plot_points : list[ndarray] = [x]
 
@@ -109,7 +109,7 @@ class Adagrad (Optim):
         return x - self.alpha / np.sqrt(self.sq_grad_acc + EPSILON) * grad_mod_callback(x)
 
 
-    def optimize (self, x: ndarray, func_callback, grad_func_callback, grad_mod_callback, is_plot : bool = False) -> ndarray | tuple[ndarray,list[ndarray]]: 
+    def optimize (self, x: ndarray, func_callback, grad_func_callback,hessian_func_callback, grad_mod_callback,  is_plot : bool = False) -> ndarray | tuple[ndarray,list[ndarray]]: 
         self.sq_grad_acc = np.zeros(x.shape)
         plot_points : list[ndarray] = [x]
 
@@ -149,7 +149,7 @@ class RMSProp(Optim):
         return x - self.alpha / np.sqrt(self.sq_grad_acc + EPSILON) * grad_mod_callback(x)
 
 
-    def optimize (self, x: ndarray, func_callback, grad_func_callback, grad_mod_callback, is_plot : bool = False) -> ndarray | tuple[ndarray,list[ndarray]]: 
+    def optimize (self, x: ndarray, func_callback, grad_func_callback,hessian_func_callback, grad_mod_callback,  is_plot : bool = False) -> ndarray | tuple[ndarray,list[ndarray]]: 
         self.sq_grad_acc = np.zeros(x.shape)
         plot_points : list[ndarray] = [x]
 
@@ -202,7 +202,7 @@ class Adam (Optim):
         return x - self.alpha / np.sqrt(second_order_corrected + EPSILON) * first_order_corrected
 
 
-    def optimize (self, x: ndarray, func_callback, grad_func_callback, grad_mod_callback, is_plot : bool = False) -> ndarray | tuple[ndarray,list[ndarray]]: 
+    def optimize (self, x: ndarray, func_callback, grad_func_callback, hessian_func_callback, grad_mod_callback,  is_plot : bool = False) -> ndarray | tuple[ndarray,list[ndarray]]: 
         self.first_order_acc = np.zeros(x.shape)
         self.second_order_acc = np.zeros(x.shape)
         plot_points : list[ndarray] = [x]
@@ -239,13 +239,12 @@ class Subgradient (Optim):
 
         if (func_callback(x_new) < self.f_best) : 
             self.f_best = func_callback(x_new)
-            print(f"F_Best : {self.f_best}")
             return x_new
 
         self.K -= 1
         return x
 
-    def optimize (self, x: ndarray, func_callback, grad_func_callback, grad_mod_callback, is_plot : bool = False) -> ndarray | tuple[ndarray,list[ndarray]]: 
+    def optimize (self, x: ndarray, func_callback, grad_func_callback, hessian_func_callback, grad_mod_callback, is_plot : bool = False) -> ndarray | tuple[ndarray,list[ndarray]]: 
         plot_points : list[ndarray] = [x]
         self.f_best = func_callback(x)
 