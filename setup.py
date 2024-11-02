0665a10cef7c52b2a492dbc9c42824c8f79ce1a6
Harsh <harsh.thunder.happy@gmail.com>
2024-11-03 00:47:23 +0530

    setup and publish updated

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("./README.md", "r") as file :
     long_description = file.read()
 
 setup (
-        name = "optimization-ima-314",
+        name = "ima-314",
         version = "0.0.1",
         long_description = long_description,
-        package_dir={"":"algorithms"},
-        packages = find_packages(where = "algorithms"),
+        package_dir={"":"./algorithms"},
+        packages = find_packages(where = "./algorithms"),
         url = "https://github.com/Harshtherocking/Optimization-IMA-314/",
         author = "Harsh & Ankur",
         license= "MIT"
